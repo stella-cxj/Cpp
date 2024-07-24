07870528fccca7d66f368f6e7f7a53448c1c33ad
xiaojing.chen <stella_cxj@163.com>
2024-07-24 21:38:45 +0800

    modify class2

--- CppPrimer/Chap1/class2.cpp
+++ CppPrimer/Chap1/class2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 class Person {
@@ -29,8 +29,81 @@ ostream &print(ostream &os, const Person &per) {
 }
 
 
-int main() {
+class Screen;
+class Win_mgr {
+public:
+    Win_mgr();
+    using ScreenIndex = vector<Screen>::size_type;
+    void clear(ScreenIndex);
+    void print();
+private:
+    vector<Screen> screens;
+};
 
+class Screen {
+friend void Win_mgr::clear(vector<Screen>::size_type);
+public:
+    typedef string::size_type pos;
+    Screen() = default;
+    Screen(unsigned wid, unsigned hit) : width(wid), height(hit), content(wid*hit, ' ') {}
+    Screen(unsigned wid, unsigned hit, char c) : width(wid), height(hit), content(wid*hit, c) {}
+    Screen &move(int r, int c) {
+        cursor = r*width + c;
+        return *this;
+    }
+    Screen &set(char c) {
+        content[cursor] = c;
+        return *this;
+    }
+    const Screen &display(ostream &os) const {
+        os << content;
+        return *this;
+    }
+    pos size() const;
+private:
+    string content;
+    unsigned width = 0, height = 0, cursor = 0;
+};
+
+Screen::pos Screen::size() const {
+    return height * width;
+}
+
+void Win_mgr::clear(ScreenIndex i) {    
+    Screen &s = screens[i];
+    s.content = string(s.height*s.width, ' ');
+}
+
+Win_mgr::Win_mgr() {
+    screens = {Screen(2, 3, 'X')};
+}
+void Win_mgr::print() {
+    for (auto i:screens)
+        i.display(cout);
+}
+
+int main() {
+    /*7.27*/
+    Screen myScreen(5,5,'X');
+    myScreen.move(4,0).set('#').display(cout);
+    cout << "\n";
+    myScreen.display(cout);
+    cout << "\n";
+    /*7.32*/
+    Win_mgr win;
+    win.print();cout << "\n";
+    win.clear(0);
+    win.print();cout << "\n";
 
     return 0;
 }
+/*7.31*/
+class Y;
+class X {
+    Y* y;
+};
+class Y {
+    X x;
+};
+
+
