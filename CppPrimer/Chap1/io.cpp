0683e15c350aadef5f2f528e2eac27da9b7f5b32
xiaojing.chen <stella_cxj@163.com>
2024-07-28 11:06:59 +0800

    modify directory

--- CppPrimer/Chap1/io.cpp
+++ CppPrimer/Chap1/io.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-
+/*1.4*/
     std::cout << "Enter two numbers:" << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
-    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+    std::cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
 
     return 0;
 }
