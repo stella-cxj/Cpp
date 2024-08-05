56f07eeb53e7adf48d4c2c346f263071bd9124c1
xiaojing.chen <stella_cxj@163.com>
2024-08-05 22:29:59 +0800

    update algorithm

--- CppPrimer/Chap10/algorithm.cpp
+++ CppPrimer/Chap10/algorithm.cpp
@@ -7,6 +7,58 @@
 
 using namespace std;
 
+/*10.9*/
+void print(const vector<string> &svec) {
+    for (auto i : svec) {cout << i << "\t";} cout << endl;
+}
+
+void elimDupl(vector<string> &svec) {
+
+    sort(svec.begin(), svec.end());
+    print(svec);
+
+    auto unique_end = unique(svec.begin(), svec.end());
+    print(svec);
+    
+    svec.erase(unique_end, svec.end());
+    print(svec);
+} 
+
+bool isShorter(const string &s1, const string &s2) {
+    return (s1.size() < s2.size());
+}
+
+/*10.12*/
+class Sales_data {
+public:
+    Sales_data() = default;
+    Sales_data(const string &s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(p*n) {}
+    Sales_data(const string &s) : bookNo(s) {}
+    string isbn() const {return bookNo;}
+
+private:
+    double avg_price() const {return units_sold ? revenue/units_sold : 0;}
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+bool compareIsbn(const Sales_data &obj1, const Sales_data &obj2) {
+    return (obj1.isbn() < obj2.isbn());
+}
+
+void print(const vector<Sales_data> &obj) {
+    for (auto i : obj) {
+        cout << i.isbn() << "\t";
+    }
+    cout << endl;
+}
+
+/*10.13*/
+bool longer_than5(const string &s) {
+    return (s.size() >= 5);
+}
+
 int main() {
     
     /*10.1*/
@@ -44,7 +96,30 @@ int main() {
     }
     cout << endl;
 
-    /**/
+    /*10.9*/
+    vector<string> svec = {"fox", "jump", "over", "fox", "red", "the", "red", "fox", "the"};
+    print(svec);
+    elimDupl(svec);
 
+    stable_sort(svec.begin(), svec.end(), isShorter);
+    print(svec);
+    
+    /*10.12*/
+    vector<Sales_data> Svec = {Sales_data("123"), 
+                               Sales_data("122343-51"),
+                               Sales_data("2343-512"),
+                               Sales_data("162343-51"),
+                               Sales_data("92343-51")};
+    print(Svec);
+    sort(Svec.begin(), Svec.end(), compareIsbn);
+    print(Svec);
+
+    /*10.13*/
+    vector<string> svec13 = {"fox", "jumeep", "owefeqver", "fox", "red", "thrweqwde", "red", "fox", "the"};
+    auto index = partition(svec13.begin(), svec13.end(), longer_than5);
+    for (auto i = svec13.begin(); i != index; i++) {
+        cout << *i << "\t";
+    }
+    cout << endl;
     return 0;
 }   
