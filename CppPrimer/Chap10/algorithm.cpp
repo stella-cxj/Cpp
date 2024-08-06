a3c255cc1812600c3f4bc32e71eb03cefe02b42a
xiaojing.chen <stella_cxj@163.com>
2024-08-06 22:03:13 +0800

    update algorithm

--- CppPrimer/Chap10/algorithm.cpp
+++ CppPrimer/Chap10/algorithm.cpp
@@ -4,8 +4,10 @@
 #include <list>
 #include <string>
 #include <numeric>
+#include <functional>
 
 using namespace std;
+using namespace std::placeholders;
 
 /*10.9*/
 void print(const vector<string> &svec) {
@@ -66,6 +68,52 @@ void biggies(vector<string> &svec, vector<string>::size_type s) {
     stable_sort(svec.begin(), svec.end(), isShorter);
 
     auto i = find_if(svec.begin(), svec.end(), [s](const string &a){return (a.size() > s);});
+    cout << "There are " << (svec.end() - i) << " words longer than " << s << " : " ;
+    for_each(i, svec.end(), [](const string &s){cout << s << "  ";});
+    cout << endl;
+}
+
+/*10.18*/    //用partition替代find_if
+void biggies2(vector<string> &svec, vector<string>::size_type s) {
+
+    elimDupl(svec);
+    stable_sort(svec.begin(), svec.end(), isShorter);
+
+    auto i = partition(svec.begin(), svec.end(), [s](const string &a){return (a.size() <= s);});
+    cout << "There are " << (svec.end() - i) << " words longer than " << s << " : " ;
+    for_each(i, svec.end(), [](const string &s){cout << s << "  ";});
+    cout << endl;
+}
+
+/*10.19*/    //用stable_partition替代partition
+void biggies3(vector<string> &svec, vector<string>::size_type s) {
+
+    elimDupl(svec);
+    stable_sort(svec.begin(), svec.end(), isShorter);
+
+    auto i = stable_partition(svec.begin(), svec.end(), [s](const string &a){return (a.size() <= s);});
+    cout << "There are " << (svec.end() - i) << " words longer than " << s << " : " ;
+    for_each(i, svec.end(), [](const string &s){cout << s << "  ";});
+    cout << endl;
+}
+
+/*20.22*/
+bool less_than(int size, const string &s) {
+    return (s.size() <= size);
+}
+
+/*10.24*/
+bool check_size(const string &s, string::size_type sz) {
+    return (s.size() <= sz);
+}
+
+/*10.25*/ //用check_size和bind重写此函数
+void biggies4(vector<string> &svec, vector<string>::size_type s) {
+
+    elimDupl(svec);
+    stable_sort(svec.begin(), svec.end(), isShorter);
+
+    auto i = partition(svec.begin(), svec.end(), bind(check_size, _1, s));
     cout << "There are " << (svec.end() - i) << " words longer than " << s << " : " ;
     for_each(i, svec.end(), [](const string &s){cout << s << "  ";});
     cout << endl;
@@ -146,6 +194,61 @@ int main() {
     /*10.16*/
     vector<string> svec16 = {"fox", "jump", "over", "fox", "red", "the", "red", "fox", "the"};
     biggies(svec16, 3);
+
+    /*10.17*/
+    Svec = {Sales_data("123"), 
+            Sales_data("122343-51"),
+            Sales_data("2343-512"),
+            Sales_data("162343-51"),
+            Sales_data("92343-51")};
+    print(Svec);
+    sort(Svec.begin(), Svec.end(), [](const Sales_data &s1, const Sales_data &s2){return (s1.isbn() < s2.isbn());});
+    print(Svec);
+
+    /*10.18*/
+    vector<string> svec18 = {"fox", "jump", "over", "fox", "red", "the", "red", "fox", "the"};
+    biggies2(svec18, 3);
+
+    /*10.19*/
+    vector<string> svec19 = {"fox", "jump", "over", "fox", "red", "the", "red", "fox", "the"};
+    biggies3(svec19, 3);
+
+    /*10.20*/
+    vector<string> svec20 = {"fox", "jumpfasdd", "over", "fofasdfa"};
+    int size = 6;
+    auto count = count_if(svec20.begin(), svec20.end(), [=](const string &s){return (s.size() > size);});
+    cout << "There're " << count << " words longer than " << size << endl;
+    
+    /*10.21*/
+    int val = 5;
+    auto f21 = [val]() mutable ->bool {
+        int ret = (val == 0);
+        if (val > 0) {
+            val--;
+        }
+        return ret;
+    };
+    for (auto i = 0; i < 7; i++) {
+        cout << "i = " << i << ", val is 0: " << f21() << endl;
+    }
+
+    /*10.22*/
+    vector<string> svec22 = {"fox", "jumpfasdd", "over", "fofasdfa"};
+    int size22 = 6;
+    auto count22 = count_if(svec22.begin(), svec22.end(), bind(less_than, size22, _1));
+    cout << "There're " << count22 << " words longer than " << size22 << endl;
+    
+    /*10.24*/
+    vector<int> ivec24 = {3,5,14,5,3,1,3,65};
+    string s24 = "abcdef";
+    auto i24 = find_if(ivec24.begin(), ivec24.end(), bind(check_size, s24, _1));
+    cout << "The first value bigger than s24 is " << *i24 << endl;
+
+    /*10.25*/
+    vector<string> svec25 = {"fox", "jump", "over", "fox", "red", "the", "red", "fox", "the"};
+    biggies4(svec25, 3);
+
+
     return 0;
 }
 
