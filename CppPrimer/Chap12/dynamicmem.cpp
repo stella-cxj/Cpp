26a053c28dded32808600aeb772210b1cc10f68e
xiaojing.chen <stella_cxj@163.com>
2024-08-12 22:55:25 +0800

    update dynamicmem

--- CppPrimer/Chap12/dynamicmem.cpp
+++ CppPrimer/Chap12/dynamicmem.cpp
@@ -2,22 +2,83 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <fstream>
 
 using namespace std;
 /*12.2*/
+class StrBlobPtr;
 class StrBlob {
 public:
     StrBlob() : data(make_shared<vector<string>>()) {}
     StrBlob(initializer_list<string> il) : data(make_shared<vector<string>>(il)) {}
+    vector<string>::size_type size() const {return data->size();}
     void push_back(const string &t) {data->push_back(t);}
     void pop_back() {data->pop_back();}
     string & front() {return data->front();}
     string & back() {return data->back();}
     string & front() const {return data->front();}
     string & back() const {return data->back();}
+    StrBlobPtr begin();
+    StrBlobPtr end();
+    StrBlobPtr begin() const;
+    StrBlobPtr end() const;
 private:
     shared_ptr<vector<string>> data;
+friend class StrBlobPtr;
 };
+/*12.19*/
+class StrBlobPtr {
+public:
+    StrBlobPtr(): curr(0) {}
+    StrBlobPtr(StrBlob &a, size_t sz = 0) : wptr(a.data), curr(sz) {}
+    StrBlobPtr(const StrBlob &a, size_t sz = 0) : wptr(a.data), curr(sz) {}
+    string & deref() const {
+        auto p = check(curr, "dereference past end");
+        return (*p)[curr];
+    }
+    StrBlobPtr& incr() {
+        check(curr, "increment past end of StrBlobPtr");
+        ++curr;
+        return *this;
+    }
+private:
+    shared_ptr<vector<string>> check(size_t i, const string &msg) const {
+        auto ret = wptr.lock();
+        if (!ret) 
+            throw runtime_error("unbound StrBlobPtr");
+        if (i >= ret->size()) 
+            throw out_of_range(msg);
+        return ret;
+    }
+    weak_ptr<vector<string>> wptr;
+    size_t curr;
+friend bool eq(const StrBlobPtr &, const StrBlobPtr &);
+};
+
+StrBlobPtr StrBlob::begin() {
+    return StrBlobPtr(*this);
+}
+StrBlobPtr StrBlob::end() {
+    auto ret = StrBlobPtr(*this, data->size());
+    return ret;
+}
+StrBlobPtr StrBlob::begin() const {
+    return StrBlobPtr(*this);
+}
+StrBlobPtr StrBlob::end() const {
+    auto ret = StrBlobPtr(*this, data->size());
+    return ret;
+}
+
+/*12.20*/
+bool eq(const StrBlobPtr &lhs, const StrBlobPtr &rhs) {
+    auto l = lhs.wptr.lock(), r = rhs.wptr.lock();
+    if (l == r) 
+        return (!r || lhs.curr == rhs.curr);
+    else    
+        return false;
+}
+
 /*12.6*/
 vector<int> * func6() {
     auto *p = new vector<int>;
@@ -58,6 +119,22 @@ void func_out7(shared_ptr<vector<int>> p) {
     cout << endl;
 }
 
+/*12.14*/ /*12.15*/
+struct connection {int a;};
+struct destination {int b;};
+connection connect(destination*) {return {0};}
+void disconnect(connection) {return;}
+void end_connection(connection *p) {disconnect(*p);}
+void f(destination &d) {
+    connection c = connect(&d);
+    shared_ptr<connection> p(&c, end_connection);
+}
+void f1(destination &d) {
+    connection c = connect(&d);
+    shared_ptr<connection> p(&c, [](connection *p){disconnect(*p);});
+}
+
+
 int main() {
     
     /*12.6*/
@@ -68,6 +145,28 @@ int main() {
     auto p7 = func7();
     func_in7(p7);
     func_out7(p7);
+
+    /*12.20*/
+    ifstream in("words");
+    string line;
+    StrBlob words;
+    while (getline(in, line)) {
+        words.push_back(line);
+    }
+    StrBlobPtr pwords(words.begin());
+    while (!eq(pwords, words.end())) {
+        cout << pwords.deref() << endl;
+        pwords.incr();
+    }
+
+    /*12.22*/
+    const StrBlob cwords = {"Hello", "world", "!"};
+    StrBlobPtr pcwords(cwords.begin());
+    while (!eq(pcwords, cwords.end())) {
+        cout << pcwords.deref() << " ";
+        pcwords.incr();
+    }
+    cout << endl;
 
     return 0;
 }
